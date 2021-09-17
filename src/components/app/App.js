f93729593fdfa97ae409e27e52b43d2e7172c160
Alex Kio <alexandermartelkio@gmail.com>
2021-09-17 12:50:16 -0400

    Add get http request

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,39 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Header } from '../header/Header';
 import { List } from '../list/List';
 import { Error } from '../error/Error';
+import { getData } from '../../utils/apiCalls';
 
 export const App = () => {
-  
+  const [articles, setArticles] = useState([]);
+  const [errorCode, setErrorCode] = useState(0);
+
+  const fetchAndCleanData = async (section) => {
+    try {
+      let fetched = await getData(section)
+      setArticles(fetched.results)
+    } catch (error) {
+      setErrorCode(Number(error.message))
+    }
+  };
+
+  useEffect(() => {
+    fetchAndCleanData('home')
+  }, [])
+
   return (
     <>
       <Header />
       <main>
         <Switch>
           <Route exact path='/' render={() =>
-            <List /> 
+            <List articles={articles} /> 
           }/>
 
           <Route exact path='/error' render={() =>
-            <Error />
+            <Error errorCode={errorCode} />
           }/>
           
           <Redirect to='/error' />
