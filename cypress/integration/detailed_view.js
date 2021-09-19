45a9bb2176e82a8b92b8f1d12d3b3d558e8bbb53
Alex Kio <alexandermartelkio@gmail.com>
2021-09-18 20:39:16 -0400

    Add test for app title

--- cypress/integration/detailed_view.js
+++ cypress/integration/detailed_view.js
@@ -1,11 +1,11 @@
 describe('Dashboard View', () => {
 
-  // beforeEach(() => {
-  //   cy.loadHome()
-  // });
+  beforeEach(() => {
+    cy.loadDetails()
+  });
 
   it('The page should have a header', () => {
-    
-  });
+    cy.get('h1').contains('New Fork Times')
+  })
 
 });
