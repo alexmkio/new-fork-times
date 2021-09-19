b49442cd4c65b04606c5d69dddfbd47d3e76672c
Alex Kio <alexandermartelkio@gmail.com>
2021-09-18 20:01:12 -0400

    Create custom Cypress command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loadHome', () => {
+  cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=1sPI8hfEhEK7GiJ4Hc2odGXpANBbiCVJ', 
+      { statusCode: 200, fixture: 'stub.json' })
+    .visit('http://localhost:3000')
+});
