fd7102693c110636bf668a8b436096c0ee505fbb
Alex Kio <alexandermartelkio@gmail.com>
2021-09-18 20:22:55 -0400

    Add test for number of article cards

--- cypress/integration/dashboard_view.js
+++ cypress/integration/dashboard_view.js
@@ -2,10 +2,29 @@ describe('Dashboard View', () => {
 
   beforeEach(() => {
     cy.loadHome()
-  });
+  })
 
   it('The page should have a header', () => {
     cy.get('h1').contains('New Fork Times')
-  });
+  })
+  
+  it('The page should have three article cards', () => {
+    cy.get('.card').should('have.length', 3)
+  })
+  
+  // it('The first article card should be by Travis Rollins', () => {
+  // })
+  
+  // it('The last article card should be by Taylor Want', () => {
+  // })
+  
+  // it('There should be a sort button that sorts the article cards by their last updated date (in descending order)', () => {
+  // })
+  
+  // it('Once the articles are sorted the user should no longer see the sort button', () => {
+  // })
+  
+  // it('Clicking on a card should take the user to a detailed page for that article', () => {
+  // })
 
-});
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,4 +2,4 @@ Cypress.Commands.add('loadHome', () => {
   cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=1sPI8hfEhEK7GiJ4Hc2odGXpANBbiCVJ', 
       { statusCode: 200, fixture: 'stub.json' })
     .visit('http://localhost:3000')
-});
+})
