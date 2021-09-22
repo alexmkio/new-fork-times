234c63ab89fbb028f246b46fb7f0178c96c12131
Alex Kio <alexandermartelkio@gmail.com>
2021-09-22 10:13:18 -0400

    Create list of conditionally rendered sorting options

--- src/components/functions/Functions.js
+++ src/components/functions/Functions.js
@@ -2,18 +2,34 @@ import PropTypes from 'prop-types';
 import './Functions.css';
 
 export const Functions = ({ sorted, sortArticles }) => {
-  let sortButton = sorted ? null : <button onClick={event => sortArticles(event)}>Sort by Last Updated</button>
+  console.log(sorted)
+  let publishedAsc = sorted.published_asc ? null : <option value={'pubAsc'}>Published - ascending</option>
+  let publishedDesc = sorted.published_desc ? null : <option value={'pubDesc'}>Published - descending</option>
+  let updatedAsc = sorted.updated_asc ? null : <option value={'upAsc'}>Updated - ascending</option>
+  let updatedDesc = sorted.updated_desc ? null : <option value={'upDesc'}>Updated - descending</option>
 
   return (
     <section className="function-container">
       <section>
-        {sortButton}
+        
+      <label>
+          Sort By
+        <select name='precipProbability'
+          onChange={event => sortArticles(event.target.value)}
+        >
+          {publishedAsc}
+          {publishedDesc}
+          {updatedAsc}
+          {updatedDesc}
+        </select>
+      </label>
+
       </section>
     </section>
   )
 }
 
 Functions.propTypes = {
-  sorted: PropTypes.bool.isRequired,
+  sorted: PropTypes.object.isRequired,
   sortArticles: PropTypes.func.isRequired
 }
